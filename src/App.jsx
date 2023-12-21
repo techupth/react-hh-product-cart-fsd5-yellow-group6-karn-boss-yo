a22f5fc944f19a50eb71a58cc33c1acee4b1d7ce
noppadon <noppadonsangthongw@gmail.com>
2023-12-21 18:30:31 +0700

    feat: cart product

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,80 @@
 import "./App.css";
+import products from "./data/products";
+import { useState } from "react";
 
 function App() {
+  const [cartProduct, setCartProduct] = useState([]);
+
+  const addProductToCart = (product) => {
+    const newCartProduct = [...cartProduct]
+    const existingProductIndex = newCartProduct.findIndex((p) => p.id === product.id);
+    console.log(existingProductIndex)
+    if (existingProductIndex !== -1) {
+      newCartProduct[existingProductIndex].quantity += 1;
+    } else {
+      newCartProduct.push({ ...product, quantity: 1 });
+    }
+    setCartProduct(newCartProduct);
+  }
+
+  const deleteCartProduct = (productIndex) => {
+    const newCartProduct = [...cartProduct]
+    newCartProduct.splice(productIndex, 1)
+    setCartProduct(newCartProduct)
+  }
+
+  const updateQuantity = (index, newQuantity) => {
+    const updateQuantity = [...cartProduct]
+    updateQuantity[index].quantity = newQuantity;
+    newQuantity === 0 ? deleteCartProduct(index) : setCartProduct(updateQuantity)
+  }
+
+  const sumPriceCartProduct = () => {
+    return cartProduct.reduce((total, item) => {
+      return total + item.price * item.quantity;
+    }, 0);
+  }
+
   return (
     <div className="App">
       <section className="product-container">
         <h1 className="product-heading">Products</h1>
         <div className="product-list">
-          <div className="product">
-            <img
-              src="http://dummyimage.com/350x350.png/dddddd/000000"
-              alt="sample name"
-            />
-            <h2>Sample name</h2>
-            <p>Sample desc</p>
-            <button>Add to cart</button>
-          </div>
+          {products.map((item, index) => {
+            return (
+              <div className="product" key={index}>
+                <img src={item.image} alt={item.name} />
+                <h2>{item.name}</h2>
+                <p>{item.description}</p>
+                <button onClick={() => {
+                  return(
+                    addProductToCart(item)
+                  )        
+                } }>Add to cart</button>
+              </div>  
+            );
+          })}
         </div>
       </section>
       <hr />
 
       <section className="cart">
-        <h1 className="cart-heading">Cart (Total Price is x Baht)</h1>
+        <h1 className="cart-heading">Cart (Total Price is {sumPriceCartProduct()} Baht)</h1>
         <div className="cart-item-list">
-          <div className="cart-item">
-            <h1>Item name: Fond - Neutral</h1>
-            <h2>Price: 160 Baht</h2>
-            <h2>Quantity: 2</h2>
-            <button className="delete-button">x</button>
-            <div className="quantity-actions">
-              <button className="add-quantity">+</button>
-              <button className="subtract-quantity">-</button>
-            </div>
-          </div>
+          {cartProduct.map((item, index) => {
+            return (
+              <div className="cart-item">
+                <h1>Item name: {item.name}</h1>
+                <h2>Price: {(item.quantity) * (item.price)} Baht</h2>
+                <h2>Quantity: {item.quantity}</h2>
+                <button className="delete-button" onClick={() => {deleteCartProduct(index)}}>x</button>
+                <div className="quantity-actions">
+                  <button className="add-quantity" onClick={() => {updateQuantity(index, item.quantity + 1)}}>+</button>
+                  <button className="subtract-quantity" onClick={() => {updateQuantity(index, item.quantity - 1)}}>-</button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </section>
     </div>
--- src/data/products.js
+++ src/data/products.js
@@ -1,4 +1,4 @@
-const products = [
+ const products = [
   {
     id: 1,
     name: "Fond - Neutral",
